934e80a96ee7685718e3c4b0ead315e4c1845083
Oystein Tveit <oystein.tveit@nordicsemi.no>
2023-07-04 16:50:03 +0200

    Initial commit

---
+++ features/usbip-dkms/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+KERNEL_VERSION="${KERNELVERSION:-}"
+
+set -euo
+
+if [ -z "$KERNEL_VERSION" ]; then
+  KERNEL_VERSION="v$(uname -r | grep -Po '\d\.\d+')"
+  if [ "$KERNEL_VERSION" == 'v' ]; then
+     >&2 echo "Could not automatically determine kernel version. Please set kernelVersion in the feature config."
+  fi
+fi
+
+apt update
+apt install -y --no-install-recommends \
+  ca-certificates \
+  dkms \
+  git \
+  "linux-headers-$(uname -r)" \
+  subversion
+
+# Trick to be able to only download the usbip folder
+svn export "https://github.com/torvalds/linux/tags/${KERNEL_VERSION}/drivers/usb/usbip" "/usr/src/usbip-${KERNEL_VERSION}"
+install -m644 dkms.conf "/usr/src/usbip-${KERNEL_VERSION}/dkms.conf"
+
+sed -i "s|#MODULE_VERSION#|${KERNEL_VERSION}|" "/usr/src/usbip-${KERNEL_VERSION}/dkms.conf"
+
+export CONFIG_USBIP_CORE=m
+export CONFIG_VHCI_HCD=m
+export CONFIG_USBIP_HOST=m
+export CONFIG_USBIP_VUDC=m
+
+dkms add -m usbip -v "${KERNEL_VERSION}"
+dkms build -m usbip -v "${KERNEL_VERSION}"
+dkms install -m usbip -v "${KERNEL_VERSION}"
